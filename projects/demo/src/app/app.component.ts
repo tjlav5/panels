277d4f2eb9029e75278cb8d3ccd0c0433e313717
TJ Lavelle <tj@lavelle.io>
2024-10-05 02:05:09 +0000

    Zoneless!

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PanelsComponent } from '../../../panels/src/public-api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PanelsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
