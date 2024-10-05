4c936990987c0901b8999000ea8cedd448fd3c92
TJ Lavelle <tj@lavelle.io>
2024-10-05 01:24:24 +0000

    Scaffold anuglar lib and demo-app

---
+++ projects/panels/src/lib/panels.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-panels',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      panels works!
+    </p>
+  `,
+  styles: ``
+})
+export class PanelsComponent {
+
+}
---
+++ projects/panels/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of panels
+ */
+
+export * from './lib/panels.service';
+export * from './lib/panels.component';
